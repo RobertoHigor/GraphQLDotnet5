40e4184bf1faf429f7a0309a8b9ef58ec0fa131a
Roberto Higor <roberto.higor@outlook.com>
2021-09-26 16:39:58 -0300

    Persistindo dados no docker

---
+++ Migrations/20210926183330_DummyData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraphQL.Migrations
+{
+    public partial class DummyData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Platform",
+                table: "Platform");
+
+            migrationBuilder.RenameTable(
+                name: "Platform",
+                newName: "Platforms");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Platforms",
+                table: "Platforms",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                "Platforms", 
+                columns: new [] {"Name", "LicenseKey"},
+                values: new object[,]
+                {
+                    {".NET 5", null},
+                    {"Docker", null},
+                    {"Windows", "LK24-424F-GWSW"}
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Platforms",
+                table: "Platforms");
+
+            migrationBuilder.RenameTable(
+                name: "Platforms",
+                newName: "Platform");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Platform",
+                table: "Platform",
+                column: "Id");
+        }
+    }
+}
